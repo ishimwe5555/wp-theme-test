2c7cddb3f4b12c0a86d677bc0bb8851ceb20503b
ishimwe5555 <35841555+ishimwe5555@users.noreply.github.com>
2020-07-30 19:03:07 +0200

    Add files via upload

---
+++ kapp-theme_ishimwe/inc/class-theme-utils.php
@@ -0,0 +1,67 @@
+<?php
+
+class Theme_Utils extends Theme_Core {
+
+	public function __construct() {
+
+	}
+
+	public function get_text() {
+		// COMPLETE THIS FUNCTION TO RETURN THE TEXT 'This is Kapp Theme' inside Theme_Core class
+	return $this->text();
+	}
+
+	/**
+	 * ------------------------------------------------------------------
+	 * Get current theme version
+	 * ------------------------------------------------------------------
+	 *
+	 * @return string
+	 */
+	public function get_theme_version() {
+		
+		return esc_html__( 'Version ', 'kapp-theme-text' ) . wp_get_theme()->Version;
+	}
+
+	/**
+	 * ------------------------------------------------------------------
+	 * Class instance
+	 * ------------------------------------------------------------------
+	 *
+	 * @return Theme_Utils
+	 */
+	public static function instance() {
+		return new self();
+	}
+
+	/**
+	 * ------------------------------------------------------------------
+	 * Greeting message
+	 * ------------------------------------------------------------------
+	 *
+	 * @return void
+	 */
+	public function greeting() {
+		#--------------------------------------------------------------#
+		return $this->get_text(); // This line should not be changed
+	}
+
+	/**
+	 * ------------------------------------------------------------------
+	 * Returns the text used inside the footer
+	 * ------------------------------------------------------------------
+	 *
+	 * @return void
+	 */
+	public function footer_text() {
+		#--------------------------------------------------------------#
+		
+	$Version_no= " version ". wp_get_theme()->Version;
+   	if (isset($Version_no)){
+
+     		return $this->get_text().$Version_no;
+}
+	return $this->get_text(); // This line should not be changed
+	      
+	}
+}
